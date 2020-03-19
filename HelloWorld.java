6cd6453be59afc6bc6036d0d96c86e7ee01c3a63
ghgud9642 <62369937+ghgud9642@users.noreply.github.com>
2020-03-19 14:41:15 +0900

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+package helloworld;
+
+public class hellowrold {
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+		System.out.println("I am a new Java programmer!");
+	}
+}
